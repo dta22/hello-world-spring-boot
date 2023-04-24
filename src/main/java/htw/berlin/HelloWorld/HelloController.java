1f517484273a5fc202969f0049f5404bcc723368
admin <s0569589@htw-berlin.de>
2023-04-24 12:21:59 +0200

    init

--- src/main/java/htw/berlin/HelloWorld/HelloController.java
+++ src/main/java/htw/berlin/HelloWorld/HelloController.java
@@ -1,14 +1,16 @@
 package htw.berlin.HelloWorld;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping("/helloworld")
-    public String index() {
-        return "Hello World!";
+    @GetMapping (path = "/")
+    public ModelAndView showHelloWorldPage () {
+        return new ModelAndView("HelloWorld");
     }
 
 }
